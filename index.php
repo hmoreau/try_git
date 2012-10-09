9681fa9e8d4ecd11dbaff85c9b812a4fcd116fd7
hmoreau <A362823@MA5161981.arpege.socgen>
2012-10-09 13:03:49 +0200

    auto-compile less files

--- index.php
+++ index.php
@@ -1,1 +1,6 @@
-<?php include 'templates/_layout.php'; ?>
+<?php
+require "less/lessc.inc.php";
+$less = new lessc;
+file_put_contents('css/bootstrap.css',$less->compileFile("less/bootstrap.less"));
+
+include 'templates/_layout.php'; ?>
