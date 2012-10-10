6d9c3e344b1a35bbef947ac6b2e3866f43fb2850
hmoreau <A362823@MA5161981.arpege.socgen>
2012-10-10 12:40:32 +0200

    typo + non obstrusive js menu

--- templates/_layout.php
+++ templates/_layout.php
@@ -30,8 +30,8 @@
 			<!-- Tab nav -->
 			<div class="tabs-below tabs-shake visible-desktop visible-tablet">
 				<ul class="nav nav-tabs">
-					<li><a href="#tab1" data-toggle="tab"><i class="icon-user"></i> Mon profil</a></li>
-					<li class="active"><a href="#tab2" data-toggle="tab"><i class="icon-bookmark"></i> Accueil</a></li>
+					<li><a href="/profile#tab1" data-toggle="tab"><i class="icon-user"></i> Mon profil</a></li>
+					<li class="active"><a href="/#tab2" data-toggle="tab"><i class="icon-bookmark"></i> Accueil</a></li>
 					<li><a href="#tab3" data-toggle="tab"><i class="icon-road"></i> Covoiturage</a></li>
 					<li><a href="#tab4" data-toggle="tab"><i class="icon-home"></i> Logements</a></li>
 					<li><a href="#tab5" data-toggle="tab"><i class="icon-shopping-cart"></i> Vente de biens</a></li>
